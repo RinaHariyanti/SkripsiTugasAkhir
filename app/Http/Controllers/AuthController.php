1ac863cb94746ec3a1acd9a43c3dd05f1fb5c683
RinaHariyanti <rinayanti1771@gmail.com>
2024-04-23 23:18:44 +0700

    Auth Fitur

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            // dd('Login successful!');
+            return redirect()->route('home')->with('success', 'Login successful!');
+        }
+        return redirect()->route('login')->with('error', 'Login failed. Please check your credentials.');
+    }
+
+    public function postRegister(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect()->route('login')->with('success', 'Registration successful! Please login.');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login')->with('success', 'Logged out successfully.');
+    }
+}
